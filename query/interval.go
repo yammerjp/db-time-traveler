3c145e0175e44a50f99458c7fc493143a6eb8d70
Keisuke Nakayama <me@yammer.jp>
2023-04-23 14:08:08 +0900

    Delete unused and deplecated function `ParsePast`

--- query/interval.go
+++ query/interval.go
@@ -7,23 +7,6 @@ import (
 	"unicode"
 )
 
-func ParsePast(str string) (string, error) {
-	str = strings.ToUpper(strings.ReplaceAll(str, " ", ""))
-	i := 0
-	for i < len(str) && unicode.IsDigit(rune(str[i])) {
-		i++
-	}
-	num, err := strconv.Atoi(str[0:i])
-	if err != nil {
-		return "", err
-	}
-	term := str[i:]
-	if term != "DAY" && term != "WEEK" && term != "MONTH" {
-		return "", errors.New("past allow DAY or WEEK or MONTH")
-	}
-	return strconv.Itoa(num) + " " + term, nil
-}
-
 func ParseInterval(past string, future string) (*Interval, error) {
 	pastNum, pastTerm, pastErr := devideNumAndTerm(past)
 	futureNum, futureTerm, futureErr := devideNumAndTerm(future)
--- query/interval_test.go
+++ query/interval_test.go
@@ -5,18 +5,6 @@ import (
 	"testing"
 )
 
-func TestParsePast(t *testing.T) {
-	expected := "1 MONTH"
-	ret, err := ParsePast("1month")
-	if err != nil {
-		t.Error(err)
-	}
-	if ret != expected {
-		fmt.Printf("expected: %s\nreturned: %s\n", expected, ret)
-		t.Error("parsePast must be return a expected statement")
-	}
-}
-
 func TestParseInterval(t *testing.T) {
 	expected := Interval{
 		IsPast: false,
