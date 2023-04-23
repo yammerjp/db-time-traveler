b6aa0e082ebfd5453d72ca8f572a8bece49336b2
Keisuke Nakayama <me@yammer.jp>
2023-04-23 14:03:31 +0900

    Add example comment in beforeAndAfter()

--- system/databaseManipulator.go
+++ system/databaseManipulator.go
@@ -9,7 +9,7 @@ import (
 
 func BeforeAndAfter(c *database.Connection, table string, interval query.Interval, stmtInWhereIn string, ignoreColumns []string) (string, error) {
 	ret := ""
-	primaryKeys, stmtInWhereIns, columns, columnValuesBefore, columnValuesAfter, err := beforeAndAfter(c, table, interval, stmtInWhereIn, ignoreColumns)
+	pks, stmtInWhereIns, columns, beforeValsArr, afterValsArr, err := beforeAndAfter(c, table, interval, stmtInWhereIn, ignoreColumns)
 	if err != nil {
 		return "", nil
 	}
@@ -17,14 +17,14 @@ func BeforeAndAfter(c *database.Connection, table string, interval query.Interva
 		if i != 0 {
 			ret += "\n"
 		}
-		for j, w := range primaryKeys {
+		for j, w := range pks {
 			if j != 0 {
 				ret += ", "
 			}
 			ret += fmt.Sprintf("%s: %s", w, v[j])
 		}
 		for j := range columns {
-			ret += fmt.Sprintf("\n  %s:\n    before: %s\n    after:  %s", columns[j], columnValuesBefore[i][j], columnValuesAfter[i][j])
+			ret += fmt.Sprintf("\n  %s:\n    before: %s\n    after:  %s", columns[j], beforeValsArr[i][j], afterValsArr[i][j])
 		}
 	}
 	return ret, nil
@@ -88,39 +88,61 @@ func buildStmtToSelectBeforeAndAfter(c *database.Connection, table string, inter
 	return query, columns, err
 }
 
+func selectBeforeAndAfter(c *database.Connection, table string, interval query.Interval, stmtInWhereIn string, ignoreColumns []string) ([][]string, []string, error) {
+	query, columns, err := buildStmtToSelectBeforeAndAfter(c, table, interval, stmtInWhereIn, ignoreColumns)
+	if err != nil {
+		return [][]string{}, columns, err
+	}
+	// SelectStatement to string
+	result, err := c.QueryExec(string(query))
+	if err != nil {
+		return [][]string{}, columns, err
+	}
+	return result, columns, nil
+}
+
 func beforeAndAfter(c *database.Connection, table string, interval query.Interval, stmtInWhereIn string, ignoreColumns []string) ([]string, [][]string, []string, [][]string, [][]string, error) {
-	primaryKeys, err := selectPrimaryKeyColumns(c, table)
+	pkNames, err := selectPrimaryKeyColumns(c, table)
 	if err != nil {
 		return []string{}, [][]string{}, []string{}, [][]string{}, [][]string{}, err
 	}
-	query, columns, err := buildStmtToSelectBeforeAndAfter(c, table, interval, stmtInWhereIn, ignoreColumns)
+	result, columnsToUpdate, err := selectBeforeAndAfter(c, table, interval, stmtInWhereIn, ignoreColumns)
 	if err != nil {
-		return primaryKeys, [][]string{}, columns, [][]string{}, [][]string{}, err
+		return pkNames, [][]string{}, columnsToUpdate, [][]string{}, [][]string{}, err
 	}
-	// SelectStatement to string
-	selectStmtReturnColumnValues, err := c.QueryExec(string(query))
-	if err != nil {
-		return primaryKeys, [][]string{}, columns, [][]string{}, [][]string{}, err
-	}
-	retPrimaryKeys := [][]string{}
-	retColumnValuesBefore := [][]string{}
-	retColumnValuesAfter := [][]string{}
-	for _, v := range selectStmtReturnColumnValues {
-		columnPrimaryKeys := []string{}
-		columnValuesBefore := []string{}
-		columnValuesAfter := []string{}
+	pkValsArr := [][]string{}
+	beforeValsArr := [][]string{}
+	afterValsArr := [][]string{}
+	/*
+		ex)
+			SELECT
+				id,
+				trial_end_date, trial_end_date - INTERVAL 1 MONTH,
+				registered_campaign_end_datetime, registered_campaign_end_datetime - INTERVAL 1 MONTH,
+				created_at, created_at - INTERVAL 1 MONTH,
+				updated_at, updated_at - INTERVAL 1 MONTH
+			FROM accounts
+			WHERE id IN ( 3 )
+	*/
+	for _, v := range result {
+		pkVals := []string{}
+		beforeVals := []string{}
+		afterVals := []string{}
 		for j, r := range v {
-			if j < len(primaryKeys) {
-				columnPrimaryKeys = append(columnPrimaryKeys, r)
-			} else if j%2 == 1 {
-				columnValuesBefore = append(columnValuesBefore, r)
+			if j < len(pkNames) {
+				// ex) id
+				pkVals = append(pkVals, r)
+			} else if (j-len(pkNames))%2 == 0 {
+				// ex) trial_end_date, registered_campaign_end_datetime, created_at, updated_at
+				beforeVals = append(beforeVals, r)
 			} else {
-				columnValuesAfter = append(columnValuesAfter, r)
+				// ex) trial_end_date - INTERVAL 1 MONTH, registered_campaign_end_datetime - INTERVAL 1 MONTH, created_at - INTERVAL 1 MONTH, updated_at - INTERVAL 1 MONTH
+				afterVals = append(afterVals, r)
 			}
 		}
-		retColumnValuesBefore = append(retColumnValuesBefore, append([]string{}, columnValuesBefore...))
-		retColumnValuesAfter = append(retColumnValuesAfter, append([]string{}, columnValuesAfter...))
-		retPrimaryKeys = append(retPrimaryKeys, append([]string{}, columnPrimaryKeys...))
+		beforeValsArr = append(beforeValsArr, append([]string{}, beforeVals...))
+		afterValsArr = append(afterValsArr, append([]string{}, afterVals...))
+		pkValsArr = append(pkValsArr, append([]string{}, pkVals...))
 	}
-	return primaryKeys, retPrimaryKeys, columns, retColumnValuesBefore, retColumnValuesAfter, nil
+	return pkNames, pkValsArr, columnsToUpdate, beforeValsArr, afterValsArr, nil
 }
